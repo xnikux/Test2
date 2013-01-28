ad46267ed4d4fc713090cb8ebf4978594d6607e6
Niku Schreiner <niku.schreiner@gmail.com>
2013-01-28 10:11:12 -0800

    foo

---
+++ gittest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var thing in new List<string> (){"cat", "dog", "cow", "chicken", "pig"})
+            {
+                Console.Write(string.Format("Here is a animal: {0}", thing));
+            }
+            
+
+        }
+    }
+}
